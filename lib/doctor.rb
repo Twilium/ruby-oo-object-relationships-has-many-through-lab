ece6e9774a05e51b916c759d7da503bc45237415
Emmanuel <rivera128@live.com>
2020-11-22 18:50:53 -0600

    commit tree cfebf8608124b8a6d02ffd25dff0d27e5c013d7e

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,1 +1,26 @@
+class Doctor
+    attr_accessor :name
 
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def appointments
+        Appointment.all
+    end
+
+    def new_appointment(date, patient)
+        Appointment.new(name, patient, self)
+    end
+
+    def patients
+        appointments.map {|appointment| appointment.patient}
+    end
+end
